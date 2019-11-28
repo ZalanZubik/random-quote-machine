2d621a72c123a1d9483d52ae11a6588d2495b923
Zalán Zubik <36884790+ZalanZubik@users.noreply.github.com>
2019-11-28 16:14:15 +0100

    Card style, background colors

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -3,23 +3,29 @@ import { Button, Card } from 'react-bootstrap';
 import { FaTwitter } from 'react-icons/fa';
 
 const QuoteMachine = ({ selectedQuote, assignNewQuoteIndex }) => {
-    const { quote, author } = selectedQuote;
+  const { quote, author } = selectedQuote;
     
-    return (
-        <Card style={{
-            width: '650px',
-            margin: 'auto',
-            textAlign: 'center'
-        }}>
-            <Card.Body>
+  return (
+    <div className="container">
+      <div id="quote-box"className="row d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
+        <Card bg="light" className="col-9 col-md-8 col-lg-6">
+          <Card.Body className="my-3 mx-2">
+            <blockquote className="blockquote text-center mb-4">
+              <p id="text" style={{ fontFamily: 'Kalam, cursive', fontSize: '1.7rem'}}>
                 {quote}
+              </p>
+              <footer id="author" className="blockquote-footer text-right">
                 {author}
-                <div>
-                    <a href={encodeURI(`https://twitter.com/intent/tweet?text="${quote}" - ${author}&hashtags=dailyquotes`)} target="_blank" rel="noopener noreferrer"><FaTwitter style={{ fontSize: '2rem' }} /></a>
-                    <Button variant="outline-success" onClick={assignNewQuoteIndex}>New Quote</Button>
-                </div>
-            </Card.Body>
+              </footer>
+            </blockquote>
+            <div className="d-flex justify-content-between">
+              <a id="tweet-quote" title="Tweet this quote!" href={encodeURI(`https://twitter.com/intent/tweet?text="${quote}" - ${author}&hashtags=dailyquotes`)} target="_blank" rel="noopener noreferrer"><FaTwitter style={{ fontSize: '2rem' }} /></a>
+              <Button id="new-quote" variant="primary" onClick={assignNewQuoteIndex}>New Quote</Button>
+            </div>
+          </Card.Body>
         </Card>
+      </div>
+    </div>
     )
 };
 
